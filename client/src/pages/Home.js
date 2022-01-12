7b56db2d2bf8c00a6551daa12fba3e2f33030aeb
Asher Bay <asherbay@gmail.com>
2022-01-12 15:50:32 -0700

    home page prototype

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,13 @@
 import React, { useContext } from 'react'
 import RenderJson from '../components/RenderJson';
 import { AuthContext } from '../providers/AuthProvider';
-import RecordTest from '../components/RecordTest'
+import Recorder from '../components/Recorder'
 const Home =  () => {
   const auth = useContext(AuthContext)
   return(
     <div>
       <h1>Home</h1>
-      <RenderJson json={auth} />
-      <RecordTest />
+      <Recorder />
     </div>
   )
 };

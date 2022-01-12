8aaa4f8f7ab123226730ac48d01ac458e2ba226f
Michael Knutson <mrknutson44@gmail.com>
2022-01-12 12:15:04 -0900

    styling changes

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,14 +1,41 @@
 import React from "react";
-import {Card, CardGroup, Container, Nav} from 'react-bootstrap'
+import {Card, CardGroup, Col, Container, Nav, Row} from 'react-bootstrap'
 
 const programmers = [
   {
-    name: "Michael Knutson", 
+    name: "Michael Knutson1", 
     image: "https://cdn10.bigcommerce.com/s-npe4l/products/1157/images/1335/B-MZ-SMSHD---HIGH__85441.1477602947.1280.1280.jpg?c=2",
     GitHub: "https://github.com/MRKnutson/",
     LinkedIn: "www.linkedin.com/in/michaelrknutson",
     email: "mrknutson44@gmail.com",
     blurb: "Some stuff about me and how great I am at Coding!"
+
+},
+{
+  name: "Michael Knutson2", 
+  image: "https://cdn10.bigcommerce.com/s-npe4l/products/1157/images/1335/B-MZ-SMSHD---HIGH__85441.1477602947.1280.1280.jpg?c=2",
+  GitHub: "https://github.com/MRKnutson/",
+  LinkedIn: "www.linkedin.com/in/michaelrknutson",
+  email: "mrknutson44@gmail.com",
+  blurb: "Some stuff about me and how great I am at Coding!"
+
+},
+{
+  name: "Michael Knutson3", 
+  image: "https://cdn10.bigcommerce.com/s-npe4l/products/1157/images/1335/B-MZ-SMSHD---HIGH__85441.1477602947.1280.1280.jpg?c=2",
+  GitHub: "https://github.com/MRKnutson/",
+  LinkedIn: "www.linkedin.com/in/michaelrknutson",
+  email: "mrknutson44@gmail.com",
+  blurb: "Some stuff about me and how great I am at Coding!"
+
+},
+{
+  name: "Michael Knutson4", 
+  image: "https://cdn10.bigcommerce.com/s-npe4l/products/1157/images/1335/B-MZ-SMSHD---HIGH__85441.1477602947.1280.1280.jpg?c=2",
+  GitHub: "https://github.com/MRKnutson/",
+  LinkedIn: "www.linkedin.com/in/michaelrknutson",
+  email: "mrknutson44@gmail.com",
+  blurb: "Some stuff about me and how great I am at Coding!"
 
 }
 ]
@@ -18,12 +45,14 @@ const AboutUs = () => {
   const renderProgrammers = () =>{
     return programmers.map((developer)=>{
       return(
-        <Card  key = {developer.name} style ={{
-          width: "21rem",
-          boxShadow: "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px"
+        <Col key = {developer.name}>
+        <Card className = "text-center" style ={{
+          width: "25rem",
+          boxShadow: "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px",
+          marginTop: "45px"
           }}>
           <Card.Img variant = "top" src = {developer.image} style = {{
-            maxHeight: "21rem", maxWidth: "21rem",
+             maxWidth: "25rem"
             }}/>
           <Card.Body>
             <Card.Title>{developer.name}</Card.Title>
@@ -47,24 +76,16 @@ const AboutUs = () => {
             </Nav>
           </Card.Footer>
         </Card>
+        </Col>
       )
     })
   };
   return (
     <Container>
       <h1>About Us</h1>
-      {renderProgrammers()}
-      {/* <CardGroup>
-        <Card>
-          <Card.Img style ={{maxHeight: "200px", maxWidth: "200px"}}/>
-          <Card.Body>
-            <Card.Title>
-              Michael Knutson
-            </Card.Title>
-            <Card.
-          </Card.Body>
-        </Card>
-      </CardGroup> */}
+      <Row md = {1} lg = {2}>
+        {renderProgrammers()}
+      </Row>
 
     </Container>
   )

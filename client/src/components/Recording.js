a0a0c60104c449b537c6d4bc2936a11611f3d09a
Ruby Reed <rubysreed@gmail.com>
2022-01-21 12:59:51 -0700

    main recording style

--- client/src/components/Recording.js
+++ client/src/components/Recording.js
@@ -1,3 +1,5 @@
+import {PrimaryColor, SecondaryColor, ActionColor, VocalHeader, VocalButton} from '../components/Styles.js'
+
 const Recording = (props) => {
     const {recording, showRecording, tags, images} = props
 
@@ -10,14 +12,15 @@ const Recording = (props) => {
     }
     
     return (
-        <div onClick={showRecording} style={{borderWidth: "1px", borderColor: "black", borderStyle: "solid"}}>
-            <h4>{recording.title}</h4>
-            <p>Duration: {recording.duration.toString().substring(0, recording.duration.toString().indexOf(".")+3)}</p> {/*limiting to 2 decimal digits */}
-            <p>Date: {recording.created_at.substring(0, recording.created_at.indexOf("T"))}</p>
-            <p>Tags: {tags.map((t)=> t.tag_text)}</p>
+        <div onClick={showRecording} style={{backgroundColor:`${SecondaryColor}`, margin:"1rem", borderRadius:"1rem", padding:"2rem"}}>
+            <h4 style={{color:"white"}}>{recording.title}</h4>
+            <p style={{color:"white"}}>Duration: {recording.duration.toString().substring(0, recording.duration.toString().indexOf(".")+3)}</p> {/*limiting to 2 decimal digits */}
+            <p style={{color:"white"}}>Date: {recording.created_at.substring(0, recording.created_at.indexOf("T"))}</p>
+            <p style={{color:"white"}}>Tags: {tags.map((t)=> t.tag_text)}</p>
             {images && images.length>0 && renderImages()}
             <br/>
-            <audio src={recording.pointer} controls style={{height: "35px", margin: "auto"}}/>
+            {/* <audio src={recording.pointer} controls style={{height: "35px", margin: "auto"}}/> */}
+        
         </div> 
         )
 }

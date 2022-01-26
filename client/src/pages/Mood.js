2e1cad66343568045ca47ba1e2c727fce8411015
DennyPlank <DennyMouzon@gmail.com>
2022-01-26 11:15:04 -0800

    datetime working for moods

--- client/src/pages/Mood.js
+++ client/src/pages/Mood.js
@@ -33,52 +33,51 @@ export default function Mood() {
         }
       }
 
-      // const formatTime = (dateTime) => {
-      //   let d = new Date(dateTime)
-      //   let hrs = d.getHours()
-      //   let mins = d.getMinutes()
-      //   // if(hrs <= 9)
-      //   // hrs = "0" + hrs
-      //   // if(mins < 10)
-      //   // mins = "0" + mins
-      //   // const time = hrs+":"+mins
-      //   if(hrs < 12){
-      //     if (mins <10){
-      //       mins = "0"+ mins
-      //     }
-      //     let time = hrs+":"+mins+" AM"
-      //     return time
-      //   } else if (hrs <13 && mins<60){
-      //     if (mins <10){
-      //       mins = "0"+ mins
-      //     }
-      //     let time = hrs+":"+mins+" PM"
-      //     return time
-      //   } else {
-      //     if (mins <10){
-      //       mins = "0"+ mins
-      //     }
-      //     let PMhrs = hrs - 12
-      //     let time = PMhrs+":"+mins+" PM"
-      //     return time;
-      //   }
-      // }
-      // const formatDate = (date) => {
-      //   var d = new Date(date),
-      //     month = "" + (d.getMonth() + 1),
-      //     day = "" + d.getDate(),
-      //     year = d.getFullYear();
-      //     if (month.length < 2) month = "0" + month;
-      //     if (day.length < 2) day = "0" + day;
+      const formatTime = (dateTime) => {
+        let d = new Date(dateTime)
+        let hrs = d.getHours()
+        let mins = d.getMinutes()
+        // if(hrs <= 9)
+        // hrs = "0" + hrs
+        // if(mins < 10)
+        // mins = "0" + mins
+        // const time = hrs+":"+mins
+        if(hrs < 12){
+          if (mins <10){
+            mins = "0"+ mins
+          }
+          let time = hrs+":"+mins+" AM"
+          return time
+        } else if (hrs <13 && mins<60){
+          if (mins <10){
+            mins = "0"+ mins
+          }
+          let time = hrs+":"+mins+" PM"
+          return time
+        } else {
+          if (mins <10){
+            mins = "0"+ mins
+          }
+          let PMhrs = hrs - 12
+          let time = PMhrs+":"+mins+" PM"
+          return time;
+        }
+      }
+      const formatDate = (date) => {
+        var d = new Date(date),
+          month = "" + (d.getMonth() + 1),
+          day = "" + d.getDate(),
+          year = d.getFullYear();
+          if (month.length < 2) month = "0" + month;
+          if (day.length < 2) day = "0" + day;
     
-      //   return (`${month}/${day}/${year}`);
-      // }
+        return (`${month}/${day}/${year}`);
+      }
 
       const renderSelectedDataForFraph = () => {
         let normalizedData = [] 
         recordings.map((r)=> {
-          // r.created_at = formatDate(r.created_at)
-          // r.created_at = formatTime(r.created_at)
+         r.created_at = (`${formatDate(r.created_at)} + ${formatTime(r.created_at)}`)
           normalizedData.push({
             title: r.title,
             uv: r.mood,
@@ -91,8 +90,8 @@ export default function Mood() {
       const renderDataForGraph = () => {
         let normalizedData = [] 
         recordings.map((r)=> {
-          // r.created_at = formatDate(r.created_at)
-          // r.created_at = formatTime(r.created_at)
+          r.created_at = formatDate(r.created_at) + " " + formatTime(r.created_at)
+          console.log(r.created_at)
           normalizedData.push({
             title: r.title,
             uv: r.mood,
@@ -119,7 +118,7 @@ export default function Mood() {
               <ResponsiveContainer width="100%" height={400}>
               <BarChart    data={dateTimeValueChoice ? renderSelectedDataForFraph() : renderDataForGraph()}>
                   <YAxis stroke="white" /> 
-                  <XAxis stroke="white" /> 
+                  <XAxis dataKey="date" stroke="white" /> 
                   {/* <Tooltip /> */}
                 <Bar dataKey="uv" 
                   fill="#ef4b4c"

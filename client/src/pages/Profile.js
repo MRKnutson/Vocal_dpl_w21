9b05f71e4243d668be5b00e0346dbceadf5a8a46
Michael Knutson <mrknutson44@gmail.com>
2022-01-28 13:07:14 -0900

    delete modal pops up to delete user

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,69 +1,94 @@
-import React, {useState, useContext, useEffect} from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import UserForm from "./UserForm";
 import { AuthContext } from "../providers/AuthProvider";
 import axios from "axios";
-import {Container} from 'react-bootstrap'
-import UserImage from '../components/UserImage';
+import { Container } from "react-bootstrap";
+import UserImage from "../components/UserImage";
 import { VocalButton, VocalHeader } from "../components/Styles";
+import DeleteUserModal from "../components/DeleteUserModal";
 
 const Profile = (props) => {
   const [showForm, setShowForm] = useState(false);
   const [showUpload, setShowUpload] = useState(false);
   const [users, setUsers] = useState([]);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-
-  const {id, email, password, handleUpdateUser, image} = useContext(AuthContext);
+  const { id, email, password, handleUpdateUser, image } =
+    useContext(AuthContext);
 
   useEffect(() => {
     console.log("mounted");
     getUsers();
-  },[]);
-  
+  }, []);
 
   const toggleUpload = () => {
     setShowUpload(!showUpload);
   };
 
   const toggleForm = () => {
-   setShowForm(!showForm);
- };
+    setShowForm(!showForm);
+  };
 
   const deleteUser = async () => {
-  let response = await axios.delete(`/api/auth`);
-  let filteredUsers = users.filter((user) => user.id !== id);
-  setUsers(filteredUsers);
-  refreshPage();
+    let response = await axios.delete(`/api/auth`);
+    let filteredUsers = users.filter((user) => user.id !== id);
+    setUsers(filteredUsers);
+    refreshPage();
   };
 
   const getUsers = async () => {
-  let response = await axios.get("/api/users");
-  setUsers(response.data)
+    let response = await axios.get("/api/users");
+    setUsers(response.data);
   };
 
-  function refreshPage(){
-  window.location.reload(false);
-  };
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
   return (
     <Container key={props.id}>
-      <VocalHeader style={{marginTop:"5rem", marginBottom:"2rem", marginLeft:"4rem"}}>My Profile</VocalHeader>
-      {image && <img style = {{width:"300px"}} src = {image}/>}
+      <VocalHeader
+        style={{ marginTop: "5rem", marginBottom: "2rem", marginLeft: "4rem" }}
+      >
+        My Profile
+      </VocalHeader>
+      {image && <img style={{ width: "300px" }} src={image} />}
       {/* <p>ID: {id}</p> */}
-      <br/>
-      <div style={{marginLeft:"6rem"}}>
-      <p style={{color:"white"}}>Email: {email}</p>
-      <p style={{color:"white"}}>Password: ****** </p>
-      <div style={{marginBottom:"1rem", marginTop:"3rem"}}>
-      {showUpload && <UserImage toggleUpload = {toggleUpload}/>}
-      {!showUpload && <VocalButton onClick = {toggleUpload}>Change Profile Picture</VocalButton>}
+      <br />
+      <div style={{ marginLeft: "6rem" }}>
+        <p style={{ color: "white" }}>Email: {email}</p>
+        <p style={{ color: "white" }}>Password: ****** </p>
+        <div style={{ marginBottom: "1rem", marginTop: "3rem" }}>
+          {showUpload && <UserImage toggleUpload={toggleUpload} />}
+          {!showUpload && (
+            <VocalButton onClick={toggleUpload}>
+              Change Profile Picture
+            </VocalButton>
+          )}
+        </div>
+        <VocalButton style={{ marginRight: "1rem" }} onClick={toggleForm}>
+          {showForm ? "Cancel" : "Update"}
+        </VocalButton>
+        {showForm && (
+          <UserForm
+            id={id}
+            email={email}
+            password={password}
+            handleUpdateUser={handleUpdateUser}
+            toggleForm={toggleForm}
+          />
+        )}
+        <VocalButton onClick={() => setShowDeleteModal(true)}>
+          Delete User
+        </VocalButton>
       </div>
-      <VocalButton style={{marginRight:"1rem"}} onClick = {toggleForm}>
-        {showForm ? "Cancel" : "Update"}
-      </VocalButton>
-      {showForm && <UserForm id = {id} email = {email} password = {password} handleUpdateUser = {handleUpdateUser} toggleForm = {toggleForm}/>}
-    <VocalButton onClick={() => deleteUser(id)}>Delete User</VocalButton>
-    </div>
+      <DeleteUserModal
+        deleteUser={deleteUser}
+        id={id}
+        show={showDeleteModal}
+        setShow={setShowDeleteModal}
+      />
     </Container>
   );
 };

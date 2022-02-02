6c8e6b67c72252ea27d49cc9b00c462511add027
Michael Knutson <mrknutson44@gmail.com>
2022-02-02 09:52:07 -0900

    new seeds and custom ordered sql

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,54 +8,57 @@
 
 require "faker"
 
-u1 = User.create(email:"test@test.com", password: 123456)
+# u1 = User.create(email:"test@test.com", password: 123456)
+recordings = ["https://res.cloudinary.com/djhlv2nfc/video/upload/v1643821441/vocal/audio/uveiya31jjuqqtmquedh.webm", "https://res.cloudinary.com/djhlv2nfc/video/upload/v1643671692/vocal/audio/mr8a3yrqsb5wcadhz2yo.webm", "https://res.cloudinary.com/djhlv2nfc/video/upload/v1643414986/vocal/audio/wwqbf9gi9bay2iwia391.webm"]
 
-5.times do
+200.times do
   rec = Recording.create(
-    title: "a title", 
-      pointer: "This is a test URL",
-      notes: "this is a test note",
-      mood: rand(1..5),
-      user_id: u1.id
+    title: Faker::Hipster.sentence(word_count: rand(2..5)),
+    pointer: recordings.sample,
+    notes: Faker::Lorem.paragraph(sentence_count: rand(2..4), supplemental: true),
+    mood: rand(1..5),
+    user_id: 3,
+    duration: Faker::Number.within(range: 1.0..304.0),
+    created_at: Faker::Time.between(from: DateTime.now - 31, to: DateTime.now + 205),
   )
-  thisTag = Tag.create(
-    text: "This is a tag"
-    )
-  RecordingTag.create(
-    recording_id: rec.id,
-    tag_id: thisTag.id
-    )
+  # thisTag = Tag.create(
+  #   text: "This is a tag"
+  #   )
+  # RecordingTag.create(
+  #   recording_id: rec.id,
+  #   tag_id: thisTag.id
+  #   )
 end
 
-5.times do
-  user = User.create(
-    first_name:Faker::Name.first_name,
-    last_name:Faker::Name.last_name,
-    email:Faker::Internet.unique.email, 
-    password: Faker::Internet.password(min_length: 6),
-  )
-    3.times do 
-    rec = Recording.create(
-      title: "#{user.first_name} and a title", 
-      pointer: "This is a test URL",
-      notes: "this is a test note",
-      mood: rand(1..5),
-      user_id: user.id
-    )
-    thisTag = Tag.create(
-      text: "This is a tag"
-      )
-    RecordingTag.create(
-      recording_id: rec.id,
-      tag_id: thisTag.id
-      )
-      2.times do 
-        Photo.create(
-        pointer: "random",
-        recording_id: rec.id
-      )
-      end
-    end
-end
+# 5.times do
+#   user = User.create(
+#     first_name:Faker::Name.first_name,
+#     last_name:Faker::Name.last_name,
+#     email:Faker::Internet.unique.email,
+#     password: Faker::Internet.password(min_length: 6),
+#   )
+#     3.times do
+#     rec = Recording.create(
+#       title: "#{user.first_name} and a title",
+#       pointer: "This is a test URL",
+#       notes: "this is a test note",
+#       mood: rand(1..5),
+#       user_id: user.id
+#     )
+#     thisTag = Tag.create(
+#       text: "This is a tag"
+#       )
+#     RecordingTag.create(
+#       recording_id: rec.id,
+#       tag_id: thisTag.id
+#       )
+#       2.times do
+#         Photo.create(
+#         pointer: "random",
+#         recording_id: rec.id
+#       )
+#       end
+#     end
+# end
 
 puts "seeded complete"

7931569d736c1c2b1a79b8e365e395c7d0824edd
Michael Knutson <mrknutson44@gmail.com>
2022-02-02 14:09:26 -0900

    responsive pictures

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,7 +8,7 @@ import UserImage from "../components/UserImage";
 import { PrimaryColor, VocalButton, UpdateButton } from "../components/Styles";
 import DeleteUserModal from "../components/DeleteUserModal";
 import UpdateUser from "../components/UpdateUser";
-import avatar from "../images/avatar.jpeg"
+import avatar from "../images/avatar.jpeg";
 
 const Profile = (props) => {
   const [showForm, setShowForm] = useState(false);
@@ -48,54 +48,103 @@ const Profile = (props) => {
     window.location.reload(false);
   }
 
-  console.log(nickname)
+  console.log(nickname);
   return (
-    <div style={{backgroundColor: "white", height:"auto",marginLeft:"8rem", marginRight:"8rem", marginTop:"5rem", marginBottom:"5rem", borderRadius:"1.5rem", padding:"2rem"}} key={props.id}>
-      <h1 style={{fontWeight:"700", marginBottom: "2rem", marginLeft: "4rem", marginTop:"2rem", color:`${PrimaryColor}` }}>My Profile</h1>
-      <div style={{display:"flex", justifyContent:"center", marginTop:"2rem"}}>
-        {image && <img style={{ width: "20rem", height:"20rem", borderRadius:"10rem", objectFit:"cover"}} src={image} />}
-        {!image && <img style={{ width: "20rem", height:"20rem", borderRadius:"10rem", objectFit:"cover"}} src={avatar} />}
+    <div
+      style={{
+        backgroundColor: "white",
+        height: "auto",
+        marginLeft: "8rem",
+        marginRight: "8rem",
+        marginTop: "5rem",
+        marginBottom: "5rem",
+        borderRadius: "1.5rem",
+        padding: "2rem",
+      }}
+      key={props.id}
+    >
+      <h1
+        style={{
+          fontWeight: "700",
+          marginBottom: "2rem",
+          marginLeft: "4rem",
+          marginTop: "2rem",
+          color: `${PrimaryColor}`,
+        }}
+      >
+        My Profile
+      </h1>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}
+      >
+        {image && (
+          <img
+            style={{
+              width: "40%",
+              height: "auto",
+              borderRadius: "50%",
+              objectFit: "cover",
+            }}
+            src={image}
+          />
+        )}
+        {!image && (
+          <img
+            style={{
+              width: "40%",
+              height: "auto",
+              borderRadius: "50%",
+              objectFit: "cover",
+            }}
+            src={avatar}
+          />
+        )}
       </div>
-      <div style={{display:"flex", justifyContent:"center"}}>
-          {showUpload && <UserImage toggleUpload={toggleUpload} />}
-          {!showUpload && (
-            <VocalButton style={{margin:"1rem"}} onClick={toggleUpload}>
-              {image ? "Change Profile Picture" : "Add Profile Picture"} 
-            </VocalButton>
-          )}
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {showUpload && <UserImage toggleUpload={toggleUpload} />}
+        {!showUpload && (
+          <VocalButton style={{ margin: "1rem" }} onClick={toggleUpload}>
+            {image ? "Change Profile Picture" : "Add Profile Picture"}
+          </VocalButton>
+        )}
       </div>
       <br />
       <div>
-        {nickname && <><p className="profile-label">Nickname</p><p className="profile-input">{nickname}</p></>}
-        <p className="profile-label">Email</p>
-        <p className="profile-input">{email}</p>
-        <p className="profile-label">Password</p>
-        <p className="profile-input">******</p>
-        <div style={{display:"flex", justifyContent:"right"}}>
-        <UpdateButton style={{ marginRight: "1rem"}} onClick={toggleForm}>
-          {showForm ? "Cancel" : "Update Info"}
-        </UpdateButton>
-        {showForm && (
-          <UpdateUser
-            id={id}
-            email={email}
-            password={password}
-            nickname={nickname}
-            handleUpdateUser={handleUpdateUser}
-            toggleForm={toggleForm}
-          />
+        {nickname && (
+          <>
+            <p className='profile-label'>Nickname</p>
+            <p className='profile-input'>{nickname}</p>
+          </>
         )}
-        <VocalButton onClick={() => setShowDeleteModal(true)}>
-          Delete User
-        </VocalButton>
-      <DeleteUserModal
-        deleteUser={deleteUser}
-        id={id}
-        show={showDeleteModal}
-        setShow={setShowDeleteModal}
-      />
+        <p className='profile-label'>Email</p>
+        <p className='profile-input'>{email}</p>
+        <p className='profile-label'>Password</p>
+        <p className='profile-input'>******</p>
+        <div style={{ display: "flex", justifyContent: "right" }}>
+          <UpdateButton style={{ marginRight: "1rem" }} onClick={toggleForm}>
+            {showForm ? "Cancel" : "Update Info"}
+          </UpdateButton>
+          {showForm && (
+            <UpdateUser
+              id={id}
+              email={email}
+              password={password}
+              nickname={nickname}
+              handleUpdateUser={handleUpdateUser}
+              toggleForm={toggleForm}
+            />
+          )}
+          <VocalButton onClick={() => setShowDeleteModal(true)}>
+            Delete User
+          </VocalButton>
+          <DeleteUserModal
+            deleteUser={deleteUser}
+            id={id}
+            show={showDeleteModal}
+            setShow={setShowDeleteModal}
+          />
+        </div>
       </div>
-    </div>
     </div>
   );
 };
